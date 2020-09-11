3bc5b232e4f308192fab782025e533f9ae80315d
cbuzzard <corwin1998@gmail.com>
2020-09-10 20:01:36 -0400

    providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuthButtonComponent } from './auth-button/auth-button.component';
+import { ApiCallsComponent } from './api-calls/api-calls.component';
+import { AuthInterceptorService } from './config/auth-interceptor.service'
+import { HttpClientModule ,HTTP_INTERCEPTORS} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthButtonComponent
+    AuthButtonComponent,
+    ApiCallsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
